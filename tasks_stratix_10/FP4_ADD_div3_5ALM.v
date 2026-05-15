80d6060a0dbd1010f14b8feb5f2cb9a6d42b06af
FilinSeny <arsen.domoded@gmail.com>
2026-05-15 05:43:43 +0000

    collected results from vm3

---
+++ tasks_stratix_10/FP4_ADD_div3_5ALM.v
@@ -0,0 +1,144 @@
+module quartus_prj (
+	dout,
+    clk,
+	din_A,
+	din_B);
+
+output  [0:0] dout;
+input 	clk;
+input  [3:0] din_A;
+input  [3:0] din_B;
+
+
+wire gnd;
+wire vcc;
+wire unknown;
+
+assign gnd = 1'b0;
+assign vcc = 1'b1;
+assign unknown = 1'bx;
+
+wire alm_0_sumout0;
+wire alm_0_sumout1;
+wire alm_0_lut5out0;
+wire alm_0_lut5out1;
+wire alm_0_lut6out;
+wire alm_0_cout;
+wire alm_1_sumout0;
+wire alm_1_sumout1;
+wire alm_1_lut5out0;
+wire alm_1_lut5out1;
+wire alm_1_lut6out;
+wire alm_1_cout;
+wire alm_2_sumout0;
+wire alm_2_sumout1;
+wire alm_2_lut5out0;
+wire alm_2_lut5out1;
+wire alm_2_lut6out;
+wire alm_2_cout;
+wire alm_3_sumout0;
+wire alm_3_sumout1;
+wire alm_3_lut5out0;
+wire alm_3_lut5out1;
+wire alm_3_lut6out;
+wire alm_3_cout;
+wire alm_4_sumout0;
+wire alm_4_sumout1;
+wire alm_4_lut5out0;
+wire alm_4_lut5out1;
+wire alm_4_lut6out;
+wire alm_4_cout;
+
+assign dout[0] = ~alm_4_sumout0;
+
+fourteennm_logic_module alm_0(
+    .a(din_A[0]),
+    .b(din_B[1]),
+    .c0(din_A[1]),
+    .d0(din_B[2]),
+    .c1(din_A[2]),
+    .d1(din_B[3]),
+    .e(din_A[2]),
+    .f(gnd),
+    .cin(gnd),
+    .sumout0(alm_0_sumout0),
+    .sumout1(alm_0_sumout1),
+    .cout(alm_0_cout),
+    .lut5out0(alm_0_lut5out0),
+    .lut5out1(alm_0_lut5out1),
+    .lut6out(alm_0_lut6out));
+defparam alm_0.lut_mask = 64'hC187CFBB23C83012;
+
+fourteennm_logic_module alm_1(
+    .a(din_A[3]),
+    .b(din_B[3]),
+    .c0(din_A[0]),
+    .d0(din_B[1]),
+    .c1(din_A[0]),
+    .d1(din_B[0]),
+    .e(vcc),
+    .f(din_A[2]),
+    .cin(alm_0_cout),
+    .sumout0(alm_1_sumout0),
+    .sumout1(alm_1_sumout1),
+    .cout(alm_1_cout),
+    .lut5out0(alm_1_lut5out0),
+    .lut5out1(alm_1_lut5out1),
+    .lut6out(alm_1_lut6out));
+defparam alm_1.lut_mask = 64'h0B026666805092FE;
+
+fourteennm_logic_module alm_2(
+    .a(gnd),
+    .b(din_A[2]),
+    .c0(din_B[2]),
+    .d0(din_B[0]),
+    .c1(din_A[1]),
+    .d1(din_B[3]),
+    .e(din_B[1]),
+    .f(din_B[3]),
+    .cin(alm_1_cout),
+    .sumout0(alm_2_sumout0),
+    .sumout1(alm_2_sumout1),
+    .cout(alm_2_cout),
+    .lut5out0(alm_2_lut5out0),
+    .lut5out1(alm_2_lut5out1),
+    .lut6out(alm_2_lut6out));
+defparam alm_2.lut_mask = 64'hA6901E94B42E5454;
+
+fourteennm_logic_module alm_3(
+    .a(gnd),
+    .b(din_A[1]),
+    .c0(din_B[3]),
+    .d0(din_B[0]),
+    .c1(din_A[3]),
+    .d1(din_B[2]),
+    .e(din_B[3]),
+    .f(din_B[1]),
+    .cin(alm_2_cout),
+    .sumout0(alm_3_sumout0),
+    .sumout1(alm_3_sumout1),
+    .cout(alm_3_cout),
+    .lut5out0(alm_3_lut5out0),
+    .lut5out1(alm_3_lut5out1),
+    .lut6out(alm_3_lut6out));
+defparam alm_3.lut_mask = 64'hEAAFE80FAEA1FFF5;
+
+fourteennm_logic_module alm_4(
+    .a(alm_1_sumout0),
+    .b(alm_3_sumout0),
+    .c0(alm_1_sumout1),
+    .d0(alm_2_lut5out0),
+    .c1(alm_3_sumout0),
+    .d1(vcc),
+    .e(alm_3_sumout0),
+    .f(alm_3_sumout0),
+    .cin(alm_3_cout),
+    .sumout0(alm_4_sumout0),
+    .sumout1(alm_4_sumout1),
+    .cout(alm_4_cout),
+    .lut5out0(alm_4_lut5out0),
+    .lut5out1(alm_4_lut5out1),
+    .lut6out(alm_4_lut6out));
+defparam alm_4.lut_mask = 64'h00000CC0ED076E4C;
+
+endmodule
